e30fca493c13aac8c17868fe34bbb24aaa5433e2
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-02-01 22:52:52 -0300

    exercicios

---
+++ Modulo01/condiçoes01.js
@@ -0,0 +1,21 @@
+var vel_p = Number(70)
+
+let s_velo = prompt("ok: ")
+
+if (s_velo > vel_p) { //condição básica no JS(composta!)
+    console.log(`você ultapassou o limite de valocidade sua velocidad atual é: ${s_velo}`)
+
+} else {
+    console.log('ok você está média!')
+}
+
+
+var pais = 'eua'
+
+if(pais == 'Brasil'){
+    console.log('você é brasileiro!!')
+
+}else{
+    console.log('você é estrangeiro!')
+
+}
